15a21cbfcae2c47fb3d30fc60eda849f569e4ef7
cclegg86 <chris@anchorpointassociates.com>
2025-05-30 18:11:46 -0700

    Delete components/ui/card.tsx

--- components/ui/card.tsx
+++
@@ -1,2 +0,0 @@
-export const Card = ({ children }) => <div className="rounded-xl shadow bg-white">{children}</div>;
-export const CardContent = ({ children, className }) => <div className={className}>{children}</div>;
